cf039fe27c0ba961791f97129777d02d92c9d67c
Marques Smith <marquesjs21@yahoo.com>
2019-12-09 18:20:38 -0800

    adding custom css

--- src/components/App.js
+++ src/components/App.js
@@ -10,24 +10,38 @@ class App extends React.Component {
         selectedVideo: null
     };
 
+    componentDidMount() {
+        this.onTermSubmit('Destiny');
+    }
+
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
             params: {
                 q: term
             }
         });
-        this.setState({ videos: response.data.items });
+        this.setState({           
+            videos: response.data.items,
+            selectedVideo: response.data.items[0] });
     };
     onVideoSelect = (video) => {
-      this.setState({ selectedVideo:video})
+        this.setState({ selectedVideo: video })
     }
 
     render() {
         return (
+           
             <div className="ui container">
+                 <h1 className="title">MYTUBE</h1>
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column"><VideoDetail video={this.state.selectedVideo} /></div>
+                        <div className="five wide column">
+                        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} /></div>
+                    </div>
+                </div>
             </div>
         );
     };
